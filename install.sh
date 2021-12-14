dc2280c270df51804225b2b3a3127f1f49c06555
epicosy <xtremedy@gmail.com>
2021-12-14 16:42:43 +0000

    database created by orbis; install script updated; requirements.txt updated

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ apt-get install -y postgresql libpq-dev
 python3 -m pip install psycopg2
 
 #sudo -u postgres -i
-/etc/init.d/postgresql start
-psql -U postgres --command "CREATE USER orbis WITH SUPERUSER PASSWORD 'orbis123';"
-createdb orbis
+#/etc/init.d/postgresql start
+#psql -U postgres --command "CREATE USER orbis WITH SUPERUSER PASSWORD 'orbis123';"
+#createdb orbis
 #exit
 
 pip3 install -r requirements.txt 2>&1
@@ -20,9 +20,10 @@ pip3 install . 2>&1
 [[ $? -eq 1 ]] && echo "[Error] Failed to install orbis." && exit 1 ;
 
 #Configs
-config_path="/etc/orbis"
-config_plugin_path="/etc/orbis/plugins.d"
-plugins_path="/var/lib/orbis/plugins/benchmark"
+main_path="~/.orbis"
+config_path=main_path + "/config"
+config_plugin_path=config_path + "/plugins.d"
+plugins_path=main_path + "/plugins"
 
 mkdir -p $config_path && cp "config/orbis.yml" $config_path
 mkdir -p $config_plugin_path && mkdir -p $plugins_path && cp -a "orbis/plugins/." $plugins_path

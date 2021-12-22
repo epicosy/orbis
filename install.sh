756f25550f2dcc9e63f54d86cde2495ac2bf28d8
epicosy <xtremedy@gmail.com>
2021-12-22 16:20:46 +0000

    updates install.sh script

--- install.sh
+++ install.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
-# Setting Timezone
+# Setting frontend and timezone
 DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
 
 # Setup postgres
 apt-get install -y postgresql libpq-dev
 python3 -m pip install psycopg2
 
-#sudo -u postgres -i
-#/etc/init.d/postgresql start
-#psql -U postgres --command "CREATE USER orbis WITH SUPERUSER PASSWORD 'orbis123';"
-#createdb orbis
-#exit
+su - postgres
+/etc/init.d/postgresql start
+psql -U postgres --command "CREATE USER orbis WITH SUPERUSER PASSWORD 'orbis123';"
+createdb orbis
+exit
+
+echo "[Success] Created psql user"
 
 pip3 install -r requirements.txt 2>&1
 [[ $? -eq 1 ]] && echo "[Error] Failed to install orbis dependencies." && exit 1 ;
 
+echo "[Success] Installed dependencies."
+
 pip3 install . 2>&1
 [[ $? -eq 1 ]] && echo "[Error] Failed to install orbis." && exit 1 ;
 
-#Configs
-main_path="~/.orbis"
-config_path="${main_path}/config"
-config_plugin_path="${config_path}/plugins.d"
-plugins_path="${main_path}/plugins"
+echo "[Success] Installed orbis."
 
-mkdir -p $config_path && cp "config/orbis.yml" $config_path
-mkdir -p $config_plugin_path && mkdir -p $plugins_path && cp -a "orbis/plugins/." $plugins_path
+mkdir -p "~/.orbis/config/plugins.d" && mkdir -p "~/.orbis/plugins" && cp "config/orbis.yml" "~/.orbis/config/"
 [[ $? -eq 1 ]] && echo "[Error] Failed to install orbis configs." && exit 1 ;
+
+echo "[Success] Created default configuration file paths."

52f5dcb25438643c1eb5721538e74ebc2d11f687
epicosy <eduard.pinconschi@tecnico.ulisboa.pt>
2022-03-15 11:12:54 +0000

    dynamic forms

--- ui/menu.py
+++ ui/menu.py
@@ -1,3 +1,7 @@
+from dataclasses import dataclass
+from pydoc import locate
+from typing import Any, List, Tuple, Callable
+
 import streamlit as st
 import requests
 import json
@@ -12,40 +16,145 @@ st.set_page_config(
      initial_sidebar_state="expanded",
  )
 
-st.markdown("""
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-""", unsafe_allow_html=True)
+#st.markdown("""
+#<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+#""", unsafe_allow_html=True)
 
-def get_card(id: str, name: str, buttons: list = [], text: str = ""):
-    btns = [f'<button class="btn btn-outline-dark"> {b} </button>' for b in buttons]
-    btns_str = '\n'.join(btns)
-    return f"""
-    <div class="card text-dark bg-light mb-3">
-        <div class="card-header">{id}</div>
-        <div class="card-body">
-            <h5 class="card-title text-dark">{name}</h5>
-            <p class="card-text">{text}</p>
-            {btns_str if btns else "<i></i>"}
-        </div>
-    </div>
-    """
+@dataclass
+class InputWidget:
+    name: str
+    key: str
+    type: str
 
-def set_selection(object, id, type):
-    st.session_state['id'] = id
-    st.session_state['object'] = object
-    st.session_state['type'] = type
 
-def checkout(id: str):
-    r = requests.post(url=f"{base_url}/checkout", json={'vid': id, 'root_dir': "/nexus"})
+#class Section:
+#    def __init__(self, name: str):
+#        self.name = name
 
-    if r.status_code == 200:
-        st.info(r.json())
+
+if 'endpoints' not in st.session_state:
+    r = requests.get(url=f"{base_url}/describe")
+    st.session_state.endpoints = r.json()
+
+if 'inputs' not in st.session_state:
+    st.session_state.inputs = {}
+
+if 'responses' not in st.session_state:
+    st.session_state.responses = {}
+
+    for el in st.session_state.endpoints.keys():
+        st.session_state.responses[el] = {}
+
+
+def list_tests(response: dict):
+    _, res = next(iter(response.items()))
+
+    cases = [f"{k}: {v['file']}" for k, v in res['oracle']['cases'].items()]
+
+    return cases
+
+
+def list_povs(response: list):
+    cases = []
+    for cmd in response:
+        for k, v in cmd['returns'].items():
+            cases.append(f"{k}: {v['file']}")
+
+    return cases
+
+
+def submit_form(endpoint: str, form_name: str, params: List[InputWidget], parse_response: Callable = None):
+    json_data = {}
+
+    for p in params:
+        #st.write(p.name, st.session_state[p.key], p.type)
+        if st.session_state[p.key]:
+            _type = locate(p.type)
+            json_data[p.name] = _type(st.session_state[p.key])
+        else:
+            json_data[p.name] = None
+
+    url = f"{base_url}/{endpoint}"
+    st.info(f"Posting: url:{url} ; data: {json_data}")
+    res = requests.post(url=url, json=json_data)
+
+    if res.status_code == 200:
+        st.session_state.responses[endpoint]['last'] = res.json()
+
+        if parse_response is not None:
+            out = parse_response(res.json())
+        else:
+            out = res.json()
+        if isinstance(out, list):
+            for el in out:
+                st.success(el)
+        else:
+            st.success(out)
     else:
-        st.error(r.status_code)
+        st.error(res.status_code)
+
+
+def create_form(endpoint: str, oid: str, oid_param: str = None, parse_response: Callable = None):
+    params = []
+    form_name = f"{oid}_{endpoint}_form"
+
+    with st.form(form_name):
+        for param, (_type, default) in st.session_state.endpoints[endpoint].items():
+            key = f"{param}_{oid}"
+
+            if oid_param and oid_param == param:
+                st.session_state[key] = oid
+                params.append(InputWidget(name=param, key=key, type=_type))
+                continue
+            t = locate(_type)
+
+            if t == str:
+                if default is None:
+                    st.text_input(param, key=key)
+                else:
+                    st.text_input(param, default, key=key)
+
+                params.append(InputWidget(name=param, type=_type, key=key))
+
+            elif t == int:
+                if default is None:
+                    st.slider(param, 0, 100, 1, key=key)
+                else:
+                    st.slider(param, 0, 100, default)
+                params.append(InputWidget(name=param, key=key, type=_type))
+
+            elif t == bool:
+                if default is None:
+                    st.checkbox(param, key=key)
+                else:
+                    st.checkbox(param, default, key=key)
+
+                params.append(InputWidget(name=param, key=key, type=_type))
+
+            elif t == list:
+                if default is None:
+                    st.text_area(param, [], key=key)
+                else:
+                    st.text_area(param, default, key=key)
+
+                params.append(InputWidget(name=param, key=key, type=_type))
+            else:
+                if not default:
+                    st.text_area(param, key=key)
+                else:
+                    st.text_area(param, default, key=key)
+                params.append(InputWidget(name=param, key=key, type=_type))
+
+        submit_btn = st.form_submit_button("Submit", on_click=submit_form,
+                                     kwargs={'endpoint': endpoint, 'form_name': form_name, 'params': params,
+                                             'parse_response': parse_response})
+        if submit_btn:
+            st.success(f"'endpoint' form for {oid} submitted.")
+
 
 with st.sidebar:
-    selected = option_menu("Orbis", ["Projects", 'Vulnerabilities', 'Instances', 'Post'],
-                           icons=['list', 'bug'], menu_icon="compass", default_index=0)
+    selected = option_menu("Orbis", ["Projects", 'Vulnerabilities', 'Instances', 'Configs'],
+                           icons=['git', 'bug', 'box-arrow-in-down', 'sliders'], menu_icon="compass", default_index=0)
 
 if selected == "Projects":
     r = requests.get(url=f"{base_url}/projects")
@@ -53,14 +162,18 @@ if selected == "Projects":
     cols = st.columns(3)
 
     for i, (id, p) in enumerate(projects.items()):
-#        card = get_card(id, p['name'], buttons = ['Checkout', 'Compile', 'Test', 'Generate'])
-#        cols[i%3].markdown(card, unsafe_allow_html=True)
         container = cols[i%3].container()
         container.header(id)
         container.subheader(p['name'])
+
+        with container.expander("Generate Tests", expanded=False):
+            create_form(endpoint='gen_tests', oid=id, oid_param='pid', parse_response=list_tests)
+
+        with container.expander("Generate POVs", expanded=False):
+            create_form(endpoint='gen_povs', oid=id, oid_param='pid', parse_response=list_povs)
+
         with container.expander("Raw Json", expanded=False):
             st.write(p)
-        container.button(id, on_click=set_selection,  kwargs={'id': id, 'object': p, 'type': 'proj'})
 
 
 if selected == "Vulnerabilities":
@@ -71,30 +184,48 @@ if selected == "Vulnerabilities":
     cols = st.columns(3)
 
     for i, (id, v) in enumerate(st.session_state.vulns.items()):
-#        card = get_card(id, v[id]['generic'][0])
-#        cols[i%3].markdown(card, unsafe_allow_html=True)
         container = cols[i%3].container()
         container.header(id)
         container.subheader(v[id]['generic'][0])
+
+        with container.expander("Checkout", expanded=False):
+            create_form(endpoint='checkout', oid=id, oid_param='vid')
+
         with container.expander("Raw Json", expanded=False):
             st.write(v)
-        container.button(id, on_click=set_selection, kwargs={'id': id, 'object': v, 'type': 'vuln'})
-        container.button("Checkout", on_click=checkout, key=f'checkout_{id}', kwargs={'id': id})
 
 
 if selected == "Instances":
     r = requests.get(url=f"{base_url}/instances")
     instances = r.json()
     cols = st.columns(3)
+    last = None
 
-    for i, (id, v) in enumerate(instances.items()):
-        card = get_card(id, v['path'], ['Delete', 'Refresh', 'Expand'])
-        cols[i%3].markdown(card, unsafe_allow_html=True)
+    if 'last' in st.session_state.responses['checkout']:
+        iid = st.session_state.responses['checkout']['last']['iid']
+        last = list(instances)[-1]
 
-if selected == "Post":
-    if 'object' in st.session_state:
-        st.write(st.session_state['object'])
-    if 'id' in st.session_state:
-        st.write(st.session_state['id'])
-    if 'type' in st.session_state:
-        st.write(st.session_state['type'])
+        if iid == instances[last]['id']:
+            st.write(instances[last])
+            container = cols[0].container()
+            container.header(instances[last]['id'])
+            container.subheader(instances[last]['path'])
+
+            with container.expander("Build", expanded=False):
+                create_form(endpoint='build', oid=iid, oid_param='iid')
+
+            with container.expander("Raw Json", expanded=False):
+                st.write(instances[last])
+
+            del instances[last]
+
+    for i, (id, instance) in enumerate(instances.items(), 1 if last else 0):
+        container = cols[i%3].container()
+        container.header(id)
+        container.subheader(instance['path'])
+
+        with container.expander("Build", expanded=False):
+            create_form(endpoint='build', oid=id, oid_param='iid')
+
+        with container.expander("Raw Json", expanded=False):
+            st.write(instance)

063c712a1ef5dce2a7e16a51a9a28b87885cb73f
epicosy <eduard.pinconschi@tecnico.ulisboa.pt>
2022-05-04 12:57:51 +0100

    updated source for cp command

--- install_securethemall_tests.sh
+++ install_securethemall_tests.sh
@@ -4,4 +4,4 @@ cd /tmp
 wget https://github.com/SecureThemAll/cb-repair/raw/master/rep_pckg.tar.gz -O tests.tar.gz
 tar -xvf tests.tar.gz
 rm tests.tar.gz
-cp -r tests/polls/* /usr/local/share/polls/
+cp -r rep_pckg/polls/* /usr/local/share/polls/
